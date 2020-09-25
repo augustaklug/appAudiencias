fb8e6d914f05b188200f0decd4ca793d680c530d
augustaklug <56510700+augustaklug@users.noreply.github.com>
2020-09-25 15:39:15 -0300

    .

--- src/main/java/br/com/infnet/appAudiencias/clients/IAudienciaClient.java
+++ src/main/java/br/com/infnet/appAudiencias/clients/IAudienciaClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(url = "https://app-audiencias-api.herokuapp.com/", name = "audienciaClient")
+@FeignClient(url = "https://app-audiencias-api.herokuapp.com", name = "audienciaClient")
 public interface IAudienciaClient {
     @GetMapping("/api/relatorioAudiencia")
     public List<Audiencia> obterLista();
